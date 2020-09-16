b996e54c6ba110ef196d5c2615aefdc6022155fa
Bastien <bastien.laine@yahoo.com>
2020-09-16 16:20:20 +0200

    Ajout fonctionnalité upload image

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,29 +1,46 @@
-const express = require('express');
 const Plat = require('../models/plats');
-const mongoose = require('mongoose');
+const formidable = require('formidable');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 exports.affichagePageAdmin = (req, res) => {
     res.render('admin');
 }
 
 exports.ajoutDePlats = (req, res) => {
-    new Plat({
-        titre: req.body.nomPlat,
-        description: req.body.descriptionPlat,
-        image: req.body.imagePlat,
-        categorie: req.body.categoriePlat,
-        prix: req.body.prixPlat,
-        highlighted: (req.body.highlighted ? true : false),
-        published: (req.body.published ? true : false)
-    }).save()
-        .then(() => console.log("Ajout d'un nouveau plat !"))
-        .catch((err) => console.log(err));
-    res.redirect('/carte/' + req.body.categoriePlat);
+    let form = new formidable.IncomingForm();
+    form.uploadDir = 'public/img';
+    form.parse(req, (err, fields, files) => {
+        let oldpath = files.file.path;
+        let newpath = "./public/img/" + files.file.name;
+        let filename = files.file.name;
+        fs.rename(oldpath, newpath, (err) => {
+            if (err) throw err;
+        });
+        new Plat({
+            titre: fields.nomPlat,
+            description: fields.descriptionPlat,
+            image: "img/" + filename,
+            categorie: fields.categoriePlat,
+            prix: fields.prixPlat,
+            highlighted: (fields.highlighted ? true : false),
+            published: (fields.published ? true : false)
+        }).save()
+            .then(() => console.log("Ajout d'un nouveau plat !"))
+            .catch((err) => console.log(err));
+        res.redirect('/carte/' + fields.categoriePlat);
+    });
+
 }
 
 exports.listePlats = (req, res) => {
     Plat.find((err, data) => {
-        if(err) throw err;
+        if (err) throw err;
         res.render('liste', { data: data });
     })
 }
